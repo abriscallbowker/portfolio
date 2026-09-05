4c85aabfc3d663010db679680c96ca190285b006
Alex Briscall Bowker <abriscallbowker@outlook.com>
2026-09-05 16:27:03 +0100

    Refine the about description

--- components/about-bio.tsx
+++ components/about-bio.tsx
@@ -4,11 +4,14 @@ export function AboutBio() {
   return (
     <div className="flex flex-col gap-5 text-body-md text-ink">
       <div className="flex flex-col gap-5 px-4">
-        <p>I currently lead Product Design at Prax.</p>
-        <p>I previously worked at Caura &amp; JPMorgan.</p>
+        <p>Currently lead Product Design at Prax Industries.</p>
+        <p>Previously worked at Caura and JPMorgan.</p>
+        <p>Also ran the digital product studio Visual Binary.</p>
         <p>
-          In my spare time, I enjoy running, making music, and cinema.
+          I first got into design by selling graphics on Shutterstock back in
+          2015.
         </p>
+        <p>In my spare time, I enjoy running, making music, and cinema.</p>
       </div>
       <SocialLinks />
     </div>
